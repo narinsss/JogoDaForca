af104adf67284df36868f28f3404340b84fe1192
narinsss <maarib03@gmail.com>
2022-07-22 11:38:51 -0300

    initial

---
+++ desafio/forca.js
@@ -0,0 +1,19 @@
+class Forca {
+
+  chutar(letra) {
+    letras = letra;
+    console.log(letras);
+   }
+
+  buscarEstado() { return ""; } // Possiveis valores: "perdeu", "aguardando chute" ou "ganhou"
+
+  buscarDadosDoJogo() {
+      return {
+          letrasChutadas: [], // Deve conter todas as letras chutadas
+          vidas: 6, // Quantidade de vidas restantes
+          palavra: [] // Deve ser um array com as letras que já foram acertadas ou o valor "_" para as letras não identificadas
+      }
+  }
+}
+
+module.exports = Forca;
